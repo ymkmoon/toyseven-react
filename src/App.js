1ed59a20c93fac45438fd98463c0b470682935ee
ymkmoon <ymkmoon@naver.com>
2022-04-27 13:40:43 +0900

    feat: Add pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from'./components/Navbar';
+import Home from'./pages/Home';
+import Station from'./pages/Station';
+import Voc from'./pages/Voc';
 
 function App() {
 
@@ -9,7 +12,9 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact />
+          <Route exact path='/' element={<Home />} />
+          <Route path='/station' element={<Station />} />
+          <Route path='/voc' element={<Voc />} />
         </Routes>
       </Router>
       </>
