45894afa3cd3186ee7059bf996bfd647a29c9ecf
ymkmoon <ymkmoon@naver.com>
2022-04-28 14:55:13 +0900

    feat: Add axios sample

--- src/pages/Station.js
+++ src/pages/Station.js
@@ -1,14 +1,32 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-class station extends Component {
+function GetData() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/toyseven/stations').then((response)=> {
+      setData(response.data);
+    })
+  }, []);
 
-    render() {
-      return (
-          <div>
-              <h3> Station </h3>
-          </div>
-      );
-    }
-  }
+  // response.data 의 데이터는 array 형식[{...}, {...}, ...] 이기 때문에
+  // data 에 바로 map 함수를 이용하면 에러가 발생한다.
+  // Object.values(data) 을 사용해 배열을 리턴받아 사용
+  const item = (Object.values(data)).map((item) => (
+    <li key={item.stationId}>
+      {item.stationName}
+    </li>
+  ));
+
+  return item;
+}
+
+function Station() {
+  const item = GetData();
+  return (<div><ul>
+    {item}
+    </ul>
+  </div>);  
+}
   
-  export default station;
+export default Station;
