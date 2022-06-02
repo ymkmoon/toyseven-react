5baffd87fde264eabc0463eea433ce56b5d6ba38
ymkmoon <ymkmoon@naver.com>
2022-06-02 16:11:04 +0900

    Update description because refactoring rest api

--- src/pages/apollo/voc/ApolloVocQuestion.js
+++ src/pages/apollo/voc/ApolloVocQuestion.js
@@ -30,7 +30,7 @@ const questionSubmitQuery = gql`
     question(body: $body) 
       @rest(
           type: "Question" 
-          path: "/voc" 
+          path: "/voc/question" 
           method: "POST"
           bodyKey: "body"
       ) {
@@ -59,7 +59,13 @@ function GetCategory() {
 }
 
 const HandleQuestionSubmit = async({body}) => {
-  const response = await client.mutate({ mutation: questionSubmitQuery, variables: {body: body} }).then(response => {
+
+  const headers = {
+    'Content-Type' : 'application/json',
+    'Authorization' : "Bearer eyJraWQiOiI1VG5rb1BwN0ZJS1NPSVhnNFY2SytNQTV2ZlV6S3cwdGp1cDRYdmw5Um9JPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJiNDI2YzFjMS0zYzk1LTQ2MDQtYjA4NC1jYTQ5ZDM2MGQyM2YiLCJjb2duaXRvOmdyb3VwcyI6WyJhcC1ub3J0aGVhc3QtMl9wNUhwSzZzNmtfR29vZ2xlIl0sImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5hcC1ub3J0aGVhc3QtMi5hbWF6b25hd3MuY29tXC9hcC1ub3J0aGVhc3QtMl9wNUhwSzZzNmsiLCJ2ZXJzaW9uIjoyLCJjbGllbnRfaWQiOiIyb28yaTc3M2ljYjUycmQ2cTY2MDlta3JhciIsIm9yaWdpbl9qdGkiOiI0NTliYTBlNy1hY2MyLTQ2NTQtYTk4My1jYzgyNjMwOTlhYTYiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6ImF3cy5jb2duaXRvLnNpZ25pbi51c2VyLmFkbWluIHBob25lIG9wZW5pZCBwcm9maWxlIGVtYWlsIiwiYXV0aF90aW1lIjoxNjU0MTMwODE2LCJleHAiOjE2NTQxNTcxMDAsImlhdCI6MTY1NDE1MzUwMCwianRpIjoiN2Y1MmZiMTUtOThmNi00ZGIwLTkzZmYtNDExYjUzOWM4MTBiIiwidXNlcm5hbWUiOiJnb29nbGVfMTE3ODA5NTkwNTI3MzQyNzc5MzU0In0.hPbyAMPHXXoYsvULlwVfbSXjETN6oBWPkKERJHDThoj23PR3zahETO2m10wiT9HQj_h2xRSpuu_ir2Im5W2p3D_cFznrvBCirMhFNpmPbazd6NfhN_W2eqldeE7uMD9VLG-f-au0QHoWeRQ6hfR_z-wvisZZS_NS3QkjuvBLBaNWq5IdCspO4hRINIks6G_SUi73oVHXMTQAK1ZOo71XJndKvqgegemFRtDMLpDmnnxXmVciOhStG06QtEfmegBaIfnTRtnKFwRNJopLc4wqLwQA3HgIEWT3mRjUUlLZJXaoMc1_YVNum14G-jsiCRiADY1tmFJovw3YuXE2BBi-5A"
+  }
+  
+  const response = await client.mutate({ mutation: questionSubmitQuery, variables: {body: body}, context: {headers: headers} }).then(response => {
     console.log('response : '+JSON.stringify(response));
   }).catch((error) => {
     console.log('error : '+error);
@@ -73,14 +79,12 @@ function ApolloVocQuestion() {
   const [title, setTitle] = useState('');
   const [email, setEmail] = useState('');
   const [content, setContent] = useState('');
-  const [username, setUsername] = useState('unknown');
   const [stationId, setStationId] = useState('ST-4');
 
   const body = {
       categoryId : categoryId,
       title: title,
       content: content,
-      username: username,
       stationId: stationId,
       email: email
     }  
--- src/pages/voc/VocQuestion.js
+++ src/pages/voc/VocQuestion.js
@@ -22,7 +22,13 @@ function GetCategory() {
 }
 
 const HandleQuestionSubmit = async({body}) => {
-  const response = await axios.post('http://127.0.0.1:8000/toyseven/voc', body).then((response) => {
+
+  const headers = {
+    'Content-Type' : 'application/json',
+    'Authorization' : "Bearer eyJraWQiOiI1VG5rb1BwN0ZJS1NPSVhnNFY2SytNQTV2ZlV6S3cwdGp1cDRYdmw5Um9JPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJiNDI2YzFjMS0zYzk1LTQ2MDQtYjA4NC1jYTQ5ZDM2MGQyM2YiLCJjb2duaXRvOmdyb3VwcyI6WyJhcC1ub3J0aGVhc3QtMl9wNUhwSzZzNmtfR29vZ2xlIl0sImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5hcC1ub3J0aGVhc3QtMi5hbWF6b25hd3MuY29tXC9hcC1ub3J0aGVhc3QtMl9wNUhwSzZzNmsiLCJ2ZXJzaW9uIjoyLCJjbGllbnRfaWQiOiIyb28yaTc3M2ljYjUycmQ2cTY2MDlta3JhciIsIm9yaWdpbl9qdGkiOiI0NTliYTBlNy1hY2MyLTQ2NTQtYTk4My1jYzgyNjMwOTlhYTYiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6ImF3cy5jb2duaXRvLnNpZ25pbi51c2VyLmFkbWluIHBob25lIG9wZW5pZCBwcm9maWxlIGVtYWlsIiwiYXV0aF90aW1lIjoxNjU0MTMwODE2LCJleHAiOjE2NTQxNTM5MjMsImlhdCI6MTY1NDE1MDMyMywianRpIjoiMzgzNDRlMTItMzcwZi00YmJhLThlNWQtZmNkMGViNzFiNDg0IiwidXNlcm5hbWUiOiJnb29nbGVfMTE3ODA5NTkwNTI3MzQyNzc5MzU0In0.FYwOJKng-eiNiixdyM5SU0vDRon675UATIDbbMPqXVGKgVu_srd5TQJYT4k1gWbACqm8FRYHUWw-L11_CF6MfGnqExBMJ0tI9WxIsLYm2ddEtBVbAl0IUTtlv032JRBJbrGQF81Gdp6c_V2ro-9xQ309N1BTTfL4d0xIDmgWi3J57SMLu7JvvAjx7RdLFohtwkNBzogvVHnxenSeqEMHwPfv6fdbKsnBbW0G4DbjPBtgAw9XBP4qMJXerdt1v_QJSeY98FuLa_kFoYJRu25RkLUVKtVIyQH6jEnrsZbmTzc_UUIikFoUsBuWAyKYH12jGUQYRqMtjLM_iQCENe-7Zg"
+  }
+
+  const response = await axios.post('http://127.0.0.1:8000/toyseven/voc/question', body, {headers: headers}).then((response) => {
     console.log('status : '+response.status);
   }).catch((error) => {
     console.log('error : '+error);
@@ -36,14 +42,12 @@ function VocQuestion() {
   const [title, setTitle] = useState('');
   const [email, setEmail] = useState('');
   const [content, setContent] = useState('');
-  const [username, setUsername] = useState('unknown');
   const [stationId, setStationId] = useState('ST-4');
 
   const body = {
       categoryId : categoryId,
       title: title,
       content: content,
-      username: username,
       email: email,
       stationId: stationId
     }
